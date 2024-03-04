fee581eeb5a0c7eadbdf3598c86f32a8f94293d6
marph625 <marvin.gnesner@gmail.com>
2024-03-04 13:57:06 +0100

    Datenbankanbindung zu Website mit Anzeige von Datensätzen

--- webp2_07/verarbeitung.php
+++ webp2_07/verarbeitung.php
@@ -8,24 +8,58 @@ $kunden_name = $_POST['name'];
 $kunden_email = $_POST['email'];
 $kunden_password = $_POST['password'];
 
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>id</th><th>name</th><th>email</th><th>password</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width: 150px; border: 1px solid black'>".parent::current()."</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>"."\n";
+    }
+}
+
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Verbindung erfolgreich!";
     // Einfache Hochkommata sind notwendig, sonst droht Leid
-    $sql = "INSERT INTO kunden (name, email, password) VALUES ('$kunden_name', '$kunden_email', '$kunden_password');";
+    $insert = "INSERT INTO kunden (name, email, password) VALUES ('$kunden_name', '$kunden_email', '$kunden_password');";
     // try-Block innerhalb des try-Blocks
     try {
         // use exec because no results are returned
-        $conn->exec($sql);
+        $conn->exec($insert);
         echo "Neuer Eintrag hinzugefügt!";
     }
     catch (PDOException $e) {
-        echo $sql."<br>".$e->getMessage();
+        echo $insert."<br>".$e->getMessage();
     }
 } catch (PDOException $e) {
     echo "Verbindung fehlgeschlagen";
 }
-
-$conn = null; 
+try {
+    // use exec because no results are returned
+    $select = $conn->prepare("SELECT * FROM kunden;");
+    $select->execute();
+    $result = $select->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($select->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+    
+} catch (PDOException $e) {
+    echo $select."<br>".$e->getMessage();
+}
+$conn = null;
+echo "</table>";
