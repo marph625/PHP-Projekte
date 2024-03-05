5214315fd9594cb6d9362bab78b3a1432b707f07
marph625 <marvin.gnesner@gmail.com>
2024-03-05 15:58:52 +0100

    Man kann jetzt nach der Anmeldung Dateien hochladen und in der Datenbank speichern

--- webp2_07/anmelden.php
+++ webp2_07/anmelden.php
@@ -9,45 +9,46 @@
 
 <body>
     <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "webp2_07";
-$tablename = "kunden";
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "webp2_07";
+        $tablename = "kunden";
 
-$kunden_name = $_POST['name'];
-$kunden_password = $_POST['password'];
+        $kunden_name = $_POST['name'];
+        $kunden_password = $_POST['password'];
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Verbindung erfolgreich!<br>";
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Verbindung erfolgreich!<br>";
 
-    $select = $conn->prepare("SELECT * FROM $tablename WHERE name = '$kunden_name'");
-    $select->execute();
+            $select = $conn->prepare("SELECT * FROM $tablename WHERE name = '$kunden_name'");
+            $select->execute();
 
-    $result = $select->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $select->setFetchMode(PDO::FETCH_ASSOC);
 
-    $dbFetch = $select->fetchAll();
-    $dbName = $dbFetch[0]['name'];
-    $dbPassword = $dbFetch[0]['password'];
-
-
-
-    if ($kunden_name == $dbName && password_verify($kunden_password, $dbPassword)) {
-        echo "Login erfolgreich!<br>";
-    } else {
-        echo "!!!ZUGANG VERWEIGERT!!!<br>";
-    }
+            $dbFetch = $select->fetchAll();
+            $dbName = $dbFetch[0]['name'];
+            $dbPassword = $dbFetch[0]['password'];
+            $dbKundenId = $dbFetch[0]['id'];
 
 
 
-} catch (PDOException $e) {
-    echo "Verbindung fehlgeschlagen<br>";
-}
+            if ($kunden_name == $dbName && password_verify($kunden_password, $dbPassword)) {
+                echo "Login erfolgreich!<br>";
+            } else {
+                echo "!!!ZUGANG VERWEIGERT!!!<br>";
+            }
+
+
+
+        } catch (PDOException $e) {
+            echo "Verbindung fehlgeschlagen<br>";
+        }
     ?>
     <form action="verarbeitung.php" method="post">
-        <input type="text" name="name">Beschreibung<br>
+        <input type="text" name="description">Beschreibung<br>
         <input type="file" name="file">Datei<br>
         <button type="submit">Datei hochladen</button><br>
     </form>
--- webp2_07/verarbeitung.php
+++ webp2_07/verarbeitung.php
@@ -4,16 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "webp2_07";
 
-
-$kunden_name = $_POST['name'];
-$kunden_email = $_POST['email'];
-// Hash Algorithmus für Passwörter
-$kunden_password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-
 // TABELLE
-
-echo "<table style='border: solid 1px black;'>";
-echo "<tr><th>id</th><th>name</th><th>email</th><th>password</th></tr>";
 
 class TableRows extends RecursiveIteratorIterator {
     
@@ -37,44 +28,107 @@ class TableRows extends RecursiveIteratorIterator {
 
 // TRY BLOCK
 
-try {
+if (isset($_POST['name'])) {
+    $kunden_name = $_POST['name'];
+    $kunden_email = $_POST['email'];
+    // Hash Algorithmus für Passwörter
+    $kunden_password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+    echo "<table style='border: solid 1px black;'>";
+    echo "<tr><th>id</th><th>name</th><th>email</th><th>password</th></tr>";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
+        // set the PDO error mode to exception
     
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
-    // set the PDO error mode to exception
-
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Verbindung erfolgreich! ";
-
-    // Einfache Hochkommata sind notwendig, sonst droht Leid
-    // prepare() um SQL-Injections vorzubeugen
-    $insert = $conn->prepare("INSERT INTO kunden (name, email, password) VALUES ('$kunden_name', '$kunden_email', '$kunden_password');");
-
-} catch (PDOException $e) {
-    echo "Verbindung fehlgeschlagen";
-}
-
-try {
-    // use exec because no results are returned
-    $insert->execute();
-    echo "Neuer Eintrag hinzugefügt! ";
-}
-catch (PDOException $e) {
-    echo $insert."<br>".$e->getMessage();
-}
-
-// Ausgabe der Tabelle kunden aus webp2_07
-try {
-    $select = $conn->prepare("SELECT * FROM kunden;");
-    $select->execute();
-    $result = $select->setFetchMode(PDO::FETCH_ASSOC);
-    foreach(new TableRows(new RecursiveArrayIterator($select->fetchAll())) as $k=>$v) {
-        echo $v;
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Verbindung erfolgreich! ";
+    
+        // Einfache Hochkommata sind notwendig, sonst droht Leid
+        // prepare() um SQL-Injections vorzubeugen
+        $insert = $conn->prepare("INSERT INTO kunden (name, email, password) VALUES ('$kunden_name', '$kunden_email', '$kunden_password');");
+    
+    
+    } catch (PDOException $e) {
+        echo "Verbindung fehlgeschlagen";
     }
     
-} catch (PDOException $e) {
-    echo $select."<br>".$e->getMessage();
+    try {
+        // use exec because no results are returned
+        $insert->execute();
+        echo "Neuer Eintrag hinzugefügt! ";
+    }
+    catch (PDOException $e) {
+        echo $insert."<br>".$e->getMessage();
+    }
+    
+    // Ausgabe der Tabelle kunden aus webp2_07
+    try {
+        $select = $conn->prepare("SELECT * FROM kunden;");
+        $select->execute();
+        $result = $select->setFetchMode(PDO::FETCH_ASSOC);
+        foreach(new TableRows(new RecursiveArrayIterator($select->fetchAll())) as $k=>$v) {
+            echo $v;
+        }
+        
+    } catch (PDOException $e) {
+        echo $select."<br>".$e->getMessage();
+    }
+    
+    
+    $conn = null;
+    echo "</table>";
 }
 
+// Datei hochladen
 
-$conn = null;
-echo "</table>";
+if (isset($_POST['file'])) {
+
+$blobby_beschreibung = $_POST['description'];
+$blobby_datei = $_POST['file'];
+
+    echo "<table style='border: solid 1px black;'>";
+    echo "<tr><th>ID</th><th>Kunden_ID</th><th>Beschreibung</th><th>Datei</th></tr>";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
+        // set the PDO error mode to exception
+    
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Verbindung erfolgreich! ";
+    
+        // Einfache Hochkommata sind notwendig, sonst droht Leid
+        // prepare() um SQL-Injections vorzubeugen
+        $insert2 = $conn->prepare("INSERT INTO blobby (beschreibung, datei) VALUES ('$blobby_beschreibung', '$blobby_datei')");
+    
+    
+    } catch (PDOException $e) {
+        echo "Verbindung fehlgeschlagen";
+    }
+    
+    try {
+        // use exec because no results are returned
+        $insert2->execute();
+        echo "Neuer Eintrag hinzugefügt! ";
+    }
+    catch (PDOException $e) {
+        echo $insert2."<br>".$e->getMessage();
+    }
+    
+    // Ausgabe der Tabelle kunden aus webp2_07
+    try {
+        $select = $conn->prepare("SELECT * FROM blobby;");
+        $select->execute();
+        $result = $select->setFetchMode(PDO::FETCH_ASSOC);
+        foreach(new TableRows(new RecursiveArrayIterator($select->fetchAll())) as $k=>$v) {
+            echo $v;
+        }
+        
+    } catch (PDOException $e) {
+        echo $select."<br>".$e->getMessage();
+    }
+    
+    
+    $conn = null;
+    echo "</table>";
+}
